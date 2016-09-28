0d36ff6ecca299c9638341e9e3e7f38ef08372b6
andremartinstt <n.y.andre2@gmail.com>
2016-09-28 14:47:26 -0300

    Criado página de Login

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="_css/estilo.css" />
+	</head>
+	<body>
+		<div id="login">
+			<h2 class="page-title">Entrar</h2>
+			<p class="form-group">
+				<label for="user">Usuário</label>
+				<input class="field" type="text" id="user" name="user" placeholder="Usuário" required="required" size="70" >
+			</p>
+			<p class="form-group">
+				<label for="password">Senha</label>
+				<input class="field" type="password" id="password" name="password" placeholder="Senha" required="required">
+			</p>
+			<p class="form-group">
+				<input type="submit" class="button" value="Entrar">
+			</p>
+		</div>
+	</body>
+</hmtl>
